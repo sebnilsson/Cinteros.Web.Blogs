c5fbef4d373bc897fb3ec630f22106719af7bb72
sebnilsson <seb.nilsson@gmail.com>
2012-05-09 11:55:58 +0200

    Cleaned up site.js.

--- src/Cinteros.Web.Blogs.Website/content/site.js
+++ src/Cinteros.Web.Blogs.Website/content/site.js
@@ -1,12 +1,21 @@
-﻿SyntaxHighlighter.autoloader(
-  'csharp c-sharp ' + basePath + 'libraries/SyntaxHighlighter/shBrushCSharp.js',
-  'css ' + basePath + 'libraries/SyntaxHighlighter/shBrushCss.js',
-  'js jscript javascript ' + basePath + 'libraries/SyntaxHighlighter/shBrushJScript.js',
-  'sql ' + basePath + 'libraries/SyntaxHighlighter/shBrushSql.js',
-  'html xhtml xml ' + basePath + 'libraries/SyntaxHighlighter/shBrushXml.js'
-);
-
-SyntaxHighlighter.all({
-    bloggerMode: true,
-    toolbar: false
+﻿$(function () {
+    initFunctions.syntaxHighlighter(appSettings.basePath);
 });
+
+var initFunctions = {
+    syntaxHighlighter: function (basePath) {
+        SyntaxHighlighter.autoloader(
+            'csharp c-sharp ' + basePath + 'libraries/SyntaxHighlighter/shBrushCSharp.js',
+            'css ' + basePath + 'libraries/SyntaxHighlighter/shBrushCss.js',
+            'js jscript javascript ' + basePath + 'libraries/SyntaxHighlighter/shBrushJScript.js',
+            'sql ' + basePath + 'libraries/SyntaxHighlighter/shBrushSql.js',
+            'html xhtml xml ' + basePath + 'libraries/SyntaxHighlighter/shBrushXml.js'
+        );
+
+        SyntaxHighlighter.all({
+            bloggerMode: true,
+            //clipboardSwf: 'http://alexgorbatchev.com/pub/sh/current/scripts/clipboard.swf',
+            toolbar: false
+        });
+    }
+};
