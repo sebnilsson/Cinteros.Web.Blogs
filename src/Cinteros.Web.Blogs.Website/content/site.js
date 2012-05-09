6d95a62537a008b3c9715a8be461195327cc8e31
sebnilsson <seb.nilsson@gmail.com>
2012-05-09 10:42:51 +0200

    Added SyntaxHighlighter to solution.

--- src/Cinteros.Web.Blogs.Website/content/site.js
+++ src/Cinteros.Web.Blogs.Website/content/site.js
@@ -1,1 +1,12 @@
-﻿
+﻿SyntaxHighlighter.autoloader(
+  'csharp c-sharp ' + basePath + 'libraries/SyntaxHighlighter/shBrushCSharp.js',
+  'css ' + basePath + 'libraries/SyntaxHighlighter/shBrushCss.js',
+  'js jscript javascript ' + basePath + 'libraries/SyntaxHighlighter/shBrushJScript.js',
+  'sql ' + basePath + 'libraries/SyntaxHighlighter/shBrushSql.js',
+  'html xhtml xml ' + basePath + 'libraries/SyntaxHighlighter/shBrushXml.js'
+);
+
+SyntaxHighlighter.all({
+    bloggerMode: true,
+    toolbar: false
+});
