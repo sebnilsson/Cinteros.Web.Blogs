6e56d8d40c9f6943966713842179e142c0411458
sebnilsson <seb.nilsson@gmail.com>
2012-06-20 15:16:46 +0200

    Changed theme for syntax-highlight.

--- src/Cinteros.Web.Blogs.Website/content/site.js
+++ src/Cinteros.Web.Blogs.Website/content/site.js
@@ -1,23 +1,24 @@
-﻿$(function () {
+﻿(function () {
     'use strict';
-    initFunctions.syntaxHighlighter(appSettings.basePath);
-});
+    $(function () {
+        initFunctions.syntaxHighlighter(appSettings.basePath);
+    });
 
-var initFunctions = {
-    syntaxHighlighter: function (basePath) {
-        'use strict';
-        SyntaxHighlighter.autoloader(
-            'csharp c-sharp ' + basePath + 'libraries/SyntaxHighlighter/shBrushCSharp.js',
-            'css ' + basePath + 'libraries/SyntaxHighlighter/shBrushCss.js',
-            'js jscript javascript ' + basePath + 'libraries/SyntaxHighlighter/shBrushJScript.js',
-            'sql ' + basePath + 'libraries/SyntaxHighlighter/shBrushSql.js',
-            'html xhtml xml ' + basePath + 'libraries/SyntaxHighlighter/shBrushXml.js'
-        );
+    var initFunctions = {
+        syntaxHighlighter: function (basePath) {
+            SyntaxHighlighter.autoloader(
+                'csharp c-sharp ' + basePath + 'libraries/SyntaxHighlighter/shBrushCSharp.js',
+                'css ' + basePath + 'libraries/SyntaxHighlighter/shBrushCss.js',
+                'js jscript javascript ' + basePath + 'libraries/SyntaxHighlighter/shBrushJScript.js',
+                'sql ' + basePath + 'libraries/SyntaxHighlighter/shBrushSql.js',
+                'html xhtml xml ' + basePath + 'libraries/SyntaxHighlighter/shBrushXml.js'
+            );
 
-        SyntaxHighlighter.all({
-            bloggerMode: true,
-            //clipboardSwf: 'http://alexgorbatchev.com/pub/sh/current/scripts/clipboard.swf',
-            toolbar: false
-        });
-    }
-};
+            SyntaxHighlighter.all({
+                bloggerMode: true,
+                //clipboardSwf: 'http://alexgorbatchev.com/pub/sh/current/scripts/clipboard.swf',
+                toolbar: false
+            });
+        }
+    };
+})();
