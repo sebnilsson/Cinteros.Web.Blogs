d5ef8e68a8abbe4c2f77e6248a569004a2c66fcc
sebnilsson <seb.nilsson@gmail.com>
2012-06-29 14:15:37 +0200

    Implemented FancyBox for images.

--- src/Cinteros.Web.Blogs.Website/content/site.js
+++ src/Cinteros.Web.Blogs.Website/content/site.js
@@ -2,10 +2,21 @@
     'use strict';
 
     $(function () {
-        
+        cinterosBlog.initFancyBox();
     });
 
     var cinterosBlog = {
-        
+        initFancyBox: function () {
+            $('.post-content a:has(img)').fancybox({
+                openEffect	: 'elastic',
+    	        closeEffect	: 'elastic',
+
+    	        helpers: {
+    	            title: {
+    	                type: 'inside'
+    	            }
+    	        }
+            });
+        }
     };
 })(window, document, undefined);
