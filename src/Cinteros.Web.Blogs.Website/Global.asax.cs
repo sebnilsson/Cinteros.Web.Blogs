c125727efa111b1ba3ad7c66449f82ae74c1366e
sebnilsson <seb.nilsson@gmail.com>
2012-07-09 13:24:48 +0200

    Minor optimization of removing unused view-engines.

--- src/Cinteros.Web.Blogs.Website/Global.asax.cs
+++ src/Cinteros.Web.Blogs.Website/Global.asax.cs
@@ -83,6 +83,9 @@ namespace Cinteros.Web.Blogs.Website {
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
 
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+
             SetupBloggerViewController();
         }
 
