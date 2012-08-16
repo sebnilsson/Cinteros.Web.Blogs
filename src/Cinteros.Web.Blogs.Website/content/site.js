d8a601d1bc1da86304a485588f3b99956ca1c396
Seb Nilsson <seb.nilsson@gmail.com>
2012-08-16 15:07:58 +0200

    Added expandible button for sliding down whole content of a blog-post. Also updated jQuery to 1.8.0.

--- src/Cinteros.Web.Blogs.Website/content/site.js
+++ src/Cinteros.Web.Blogs.Website/content/site.js
@@ -2,20 +2,59 @@
     'use strict';
 
     $(function () {
+        cinterosBlog.initExpandPost();
         cinterosBlog.initFancyBox();
     });
 
     var cinterosBlog = {
+        maxPostHeight: 300,
+        initExpandPost: function () {
+            $('.post-content').each(function (index, item) {
+                var $item = $(item);
+                var itemHeight = $item.height();
+                if (itemHeight > cinterosBlog.maxPostHeight) {
+                    $('.post-comment', $item.parent()).hide();
+
+                    $item.addClass('expandible').height(cinterosBlog.maxPostHeight);
+                    $item.attr('data-org-height', itemHeight);
+
+                    $item.after('<div class="post-expand"><span class="post-expand-button"><a href="#">Visa hela</a></span></div>');
+                }
+            });
+
+            $('.post-expand-button').click(function (event) {
+                event.preventDefault();
+
+                var $button = $(this);
+                var $comments = $button.parent().next('.post-comment');
+                var $postContent = $button.parent().prev('.post-content');
+
+                var orgHeight = $postContent.attr('data-org-height');
+
+                $postContent.removeClass('expandible');
+                $postContent.animate({
+                    height: orgHeight
+                }, 300);
+
+                $button.parent().remove();
+                $comments.show();
+
+                setTimeout(function () {
+                    $postContent.height('');
+                }, 1000);
+            });
+        },
+
         initFancyBox: function () {
             $('.post-content a:has(img)').fancybox({
-                openEffect	: 'elastic',
-    	        closeEffect	: 'elastic',
+                openEffect: 'elastic',
+                closeEffect: 'elastic',
 
-    	        helpers: {
-    	            title: {
-    	                type: 'inside'
-    	            }
-    	        }
+                helpers: {
+                    title: {
+                        type: 'inside'
+                    }
+                }
             });
         }
     };
